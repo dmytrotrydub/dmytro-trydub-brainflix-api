2433957eea6dd64318151a6d32385628dc7703a4
dmytrotrydub <tridub.d87@gmail.com>
2022-06-26 11:39:17 -0400

    Created operator file for performing functional of erver

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+// const videoList = require("./data/videos.json");
+
+app.use(express.json());
+app.use(cors());
+
+console.log();
+
+app.use("/",)
+// const someData = videoList.map(videos => {
+    
+// })
---
+++ routes/videos.js
@@ -0,0 +1,5 @@
+const router = express.Router();
+const express = require("express");
+const app = express();
+const videos = require("../data/videos.json");
+
